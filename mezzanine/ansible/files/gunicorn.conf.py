0e7a01c8545f22e5b4d08e1ee39b31ebe24e9259
Lorin Hochstein <lorin.hochstein@sendgrid.com>
2014-12-25 20:56:12 -0500

    gunicorn: bind to every port

--- mezzanine/ansible/files/gunicorn.conf.py
+++ mezzanine/ansible/files/gunicorn.conf.py
@@ -2,7 +2,7 @@ from __future__ import unicode_literals
 import multiprocessing
 import os
 
-bind = "127.0.0.1:{}".format(os.environ["GUNICORN_PORT"])
+bind = "0.0.0.0:{}".format(os.environ["GUNICORN_PORT"])
 workers = multiprocessing.cpu_count() * 2 + 1
 loglevel = "error"
 proc_name = "mezzanine"
